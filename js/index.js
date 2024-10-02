17e4f930ea0ecd48933cc8b5bfc97c8351cacf19
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-10-02 18:04:45 +0500

    All codes write

---
+++ js/index.js
@@ -0,0 +1,221 @@
+// 1-Misol
+
+// let num = +prompt('Sonni kiriting');
+// let res;
+// if (num > 0) {
+//     res = ++num
+// } else {
+//     res = 'Bu butun son emas'
+// }
+
+// console.log(res);
+
+
+
+
+// 2-Misol
+
+// let num = +prompt('Sonni kiriting');
+// let res;
+// if (num > 0) {
+//     res = ++num
+// } else {
+//     res = num -= 2
+// }
+
+// console.log(res);
+
+
+
+
+// 3-Misol
+
+// let num = +prompt('Sonni kiriting');
+// let res;
+// if (num > 0) {
+//     res = ++num
+// } else if (num < 0) {
+//     res = num -= 2
+// } else {
+//     res = num += 10
+// }
+
+// console.log(res);
+
+
+
+
+// 4-Misol
+
+// let num1 = +prompt('1-sonni kiriting');
+// let num2 = +prompt('2-sonni kiriting');
+// let num3 = +prompt('3-sonni kiriting');
+
+// let musbat = 0;
+
+// if (num1 > 0) {
+//     musbat++
+// }
+
+// if (num2 > 0) {
+//     musbat++
+// }
+
+// if (num3 > 0) {
+//     musbat++
+// }
+
+// console.log(musbat);
+
+
+
+
+// 5-Misol
+
+// let num1 = +prompt('1-sonni kiriting');
+// let num2 = +prompt('2-sonni kiriting');
+// let num3 = +prompt('3-sonni kiriting');
+
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (num1 > 0) {
+//     musbat++
+// } else {
+//     manfiy++
+// }
+
+// if (num2 > 0) {
+//     musbat++
+// } else {
+//     manfiy++
+// }
+
+// if (num3 > 0) {
+//     musbat++
+// } else {
+//     manfiy++
+// }
+
+// console.log('Bu sonlar orasida' + " " + musbat + " " + "musbat son bor");
+// console.log('Bu sonlar orasida' + " " + manfiy + " " + "manfiy son bor");
+
+
+
+
+// 6-Misol
+
+// let num1 = +prompt('1-sonni kiriting');
+// let num2 = +prompt('2-sonni kiriting');
+// let res;
+
+// if (num1 > num2) {
+//     console.log("Bu yerda eng kotta son" + " " + num1);
+// } else {
+//     console.log("Bu yerda eng kotta son" + " " + num2);
+// }
+
+
+
+
+// 7-Misol
+
+// let num1 = +prompt('1-sonni kiriting');
+// let num2 = +prompt('2-sonni kiriting');
+// let res;
+
+// if (num1 < num2) {
+//     console.log("Bu yerda eng kichik son" + " " + num1);
+// } else {
+//     console.log("Bu yerda eng kichik son" + " " + num2);
+// }
+
+
+
+
+// 8-Misol
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let max;
+// let min;
+
+// if (num1 > num2 || num1 < num2) {
+//     max = num1
+//     min = num2
+// } else {
+//     max = num2
+//     min = num1
+// }
+
+// console.log("Bu yerda eng kotta son" + " " + max);
+// console.log("Bu yerda eng kichik son" + " " + min);
+
+
+
+
+// 9-Misol
+
+// let a = +prompt('1-son kiriting');
+// let b = +prompt('2-son kiriting');
+// let c;
+
+// if (a > b) {
+//     c = a;
+//     a = b;
+//     b = c;
+// }
+
+// console.log(a);
+// console.log(b);
+
+
+
+
+// 10-Misol
+
+// let a = +prompt('1-sonni kiriting');
+// let b = +prompt('2-sonni kiriting');
+// let res;
+
+// if (a != b) {
+//     res = a + b;
+//     a = res;
+//     b = res;
+//     console.log(a, b);
+// } else {
+//     a = 0;
+//     b = 0;
+//     console.log(a, b);
+// }
+
+
+
+
+// 11-Misol
+
+// let a = +prompt('1-sonni kiriting');
+// let b = +prompt('2-sonni kiriting');
+// let max;
+
+// if (a != b) {
+//     if (a > b) {
+//         max = a;
+//     } else {
+//         max = b;
+//     }
+
+//     a = max;
+//     b = max;
+//     console.log(a, b);
+// } else {
+//     a = 0;
+//     b = 0;
+//     console.log(a, b);
+// }
+
+
+
+
+// 12-Misol
+
